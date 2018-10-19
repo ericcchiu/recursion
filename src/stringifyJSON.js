f7795a387ccb1659aa98cdc28dde98ab1f718496
ericcchiu <eric.chn.chiu@gmail.com>
2018-10-19 14:20:32 -0500

    [feat] Add JSON stringify function that transforms all primitive data types including array.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,45 @@
 // this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
+// let stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+const stringifyJSON = function(obj) {
+  const stringedArr = [];
+
+  // Check for primitive types 
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';  
+  } else if (typeof obj === 'number' || obj === null || typeof obj === 'boolean') { 
+    return '' + obj;
+  }
+  // Check if object is an array  
+  if (Array.isArray(obj)) { 
+    if (obj.length === 0) { 
+      return '[]'; 
+    } else { 
+      obj.forEach( function (elem) { 
+        stringedArr.push(stringifyJSON(elem));  
+      }); 
+        return '[' + stringedArr + ']'; 
+    }
+  }
+  
+  /***Check if obj is an Object***/ 
+  if (typeof obj === 'object' && !Array.isArray(obj)) { 
+
+    let listOfKeys = Object.keys(obj);
+    let result = [];
+
+    listOfKeys.forEach(function (key) {
+      let keys = '"' + key + '":'; 
+      // Recursive case
+      let val = stringifyJSON(obj[key]);
+      
+      if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+        result.push(keys + val);
+      }
+    });
+
+    return "{" + result.join(",") + "}";
+  }
 };
