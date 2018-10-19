afa699fdf2efef2c7065803684f77ce73d4a394c
ericcchiu <eric.chn.chiu@gmail.com>
2018-10-19 09:15:59 -0500

    [feat] Add getElementsByClassName function.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+const getElementsByClassName = (className, node) => {
+  // Store matching results in a list 
+  let listOfMatches = []; 
+
+  // Track the node we are currently on 
+  node = node || document.body; 
+
+  let classList = node.className.split(' '); 
+
+  // Base case: Check if the classList includes the current className push to list if true 
+  if (classList.includes(className)) { 
+    listOfMatches.push(node); 
+  } 
+
+  // Iterate over the child nodes 
+  for (let i = 0; i < node.children.length; i++) { 
+    // Recursive call 
+    let childNode = getElementsByClassName(className, node.children[i]); 
+    // console.log(childNode); 
+
+    listOfMatches = listOfMatches.concat(childNode); 
+  }
+  // Return matching list 
+  return listOfMatches; 
 };
